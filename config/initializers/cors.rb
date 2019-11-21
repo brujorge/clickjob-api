7c044f0c375e7320a84fd8fbcf8f1878e5210252
brujorge <brujorgegonzales@gmail.com>
2019-11-21 17:17:42 -0500

    Enable cors for client

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins "http://clickjob.herokuapp.com"
+    origins "https://clickjob.herokuapp.com"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
